addaae32c6ad69009c7b0c133e2832f9418d069c
sahana <sahanadhilfer@gmail.com>
2021-01-12 18:57:06 +0100

    Entity Framework with bug

---
+++ lextraining datasql/Datacontext.cs
@@ -0,0 +1,20 @@
+﻿using lextraining_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lextraining_datasql
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
